ead7d62ec60ec687b44397b48c41a9237ff20a13
fareedagha <fareedagha7440@gmail.com>
2024-01-07 13:02:57 +0500

    added empty route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './component/login/login.component';
 import { SignupComponent } from './component/signup/signup.component';
 
 const routes: Routes = [
+  { path: '',  component: LoginComponent},
   { path: 'login',  component: LoginComponent},
   { path: 'signup',  component: SignupComponent},
 ];
