2a45a77908e4b99423a19419df66234dd1851e2a
fareedagha <fareedagha7440@gmail.com>
2024-01-08 14:20:06 +0500

    created wallet component

--- src/app/page/page.routing.module.ts
+++ src/app/page/page.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './page.component';
 import { ProductComponent } from './product/product.component';
+import { WalletComponent } from './wallet/wallet.component';
 
 const routes: Routes = [
     {
@@ -10,6 +11,8 @@ const routes: Routes = [
         children: [
             { path: '', component: ProductComponent },
             { path: 'products', component: ProductComponent },
+            { path: 'wallet', component: WalletComponent },
+
         ]
     }
 
